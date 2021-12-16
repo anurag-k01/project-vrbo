dec24740d335ba2cad08a90b4175c99dee331b03
Ravinder Singh Negi <rvnegi786@gmail.com>
2021-12-16 22:09:25 +0530

    routing

--- client/vrbo/src/App.js
+++ client/vrbo/src/App.js
@@ -1,14 +1,17 @@
-import logo from "./logo.svg";
+/** @format */
+
 import "./App.css";
 
 import Homepage from "./HomePage/Homepage";
-
+import Hotel from "./Hotel/Hotel";
+import { Routes, Route } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <Homepage />
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Homepage />}></Route>
+			<Route path='/hotel' element={<Hotel />}></Route>
+		</Routes>
+	);
 }
 
 export default App;
--- client/vrbo/src/HomePage/Homepage.js
+++ client/vrbo/src/HomePage/Homepage.js
@@ -3,33 +3,35 @@ import BookingSearcBar from "../Components/BookingSearcBar";
 import Navbar from "../Components/Navbar";
 
 import AppBody from "../AppBody/AppBody";
+import { Link } from "react-router-dom";
 function Homepage() {
-  document.title =
-    "Vrbo | Book your vacation rentals: beach houses, cabins, condos & more";
-  return (
-    <>
-      <Navbar />
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <img src="./carousal.jpg" alt="Carousal" style={{ width: "100%" }} />
-      </div>
-      {/*<BookingSearcBar />*/}
-      {/* REcent Clicked */}
-      {/* Recommened for you */}
-      {/* Recommened Destinations */}
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          position: "relative",
-          marginTop: "-40px",
-        }}
-      >
-        <AppBody />
-        {/* Get Inspired for a family trip */}
-        {/* Find Spaces that suit your style */}
-      </div>
-    </>
-  );
+	document.title =
+		"Vrbo | Book your vacation rentals: beach houses, cabins, condos & more";
+	return (
+		<>
+			<Link to='/'>
+				<Navbar />
+				<div style={{ display: "flex", justifyContent: "center" }}>
+					<img src='./carousal.jpg' alt='Carousal' style={{ width: "100%" }} />
+				</div>
+				{/*<BookingSearcBar />*/}
+				{/* REcent Clicked */}
+				{/* Recommened for you */}
+				{/* Recommened Destinations */}
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						position: "relative",
+						marginTop: "-40px",
+					}}>
+					<AppBody />
+					{/* Get Inspired for a family trip */}
+					{/* Find Spaces that suit your style */}
+				</div>
+			</Link>
+		</>
+	);
 }
 
 export default Homepage;
